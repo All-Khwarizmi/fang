d20f79b89931fb98c3210608077dc6b93d5f46dc
Jason Suarez <swarecito@gmail.com>
2023-04-13 18:07:57 +0200

    setting up

--- .history/pages/index_20230413180454.tsx
+++ .history/pages/index_20230413180454.tsx
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Sabaydi
+          Welcome to Sabaiy
         </h1>
 
         <p className={styles.description}>
